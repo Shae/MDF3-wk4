8062268219f77f85edf7abeb1200ebcebe2278da
Shae <shae@fullsail.com>
2013-02-26 08:13:28 -0800

    small layout changes

--- src/com/klusman/mdf3_wk4/MainActivity.java
+++ src/com/klusman/mdf3_wk4/MainActivity.java
@@ -6,11 +6,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -18,6 +23,9 @@ public class MainActivity extends Activity {
 	Context _context;
 	String data;
 	WebView webview;
+	int numA;
+	int numB;
+	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +42,107 @@ public class MainActivity extends Activity {
         webview.loadUrl("file:///android_asset/index.html");
         webview.addJavascriptInterface(new WebAppInterface(), "Android");
         
+        Button num0 = (Button)findViewById(R.id.btn0);
+        Button num1 = (Button)findViewById(R.id.btn1);
+        Button num2 = (Button)findViewById(R.id.btn2);
+        Button num3 = (Button)findViewById(R.id.btn3);
+        Button num4 = (Button)findViewById(R.id.btn4);
+        Button num5 = (Button)findViewById(R.id.btn5);
+        Button num6 = (Button)findViewById(R.id.btn6);
+        Button num7 = (Button)findViewById(R.id.btn7);
+        Button num8 = (Button)findViewById(R.id.btn8);
+        Button num9 = (Button)findViewById(R.id.btn9);
         
+        num0.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+        
+        num1.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+	  
+        num2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num4.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num5.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num6.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num7.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num8.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	  
+        num9.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
     }
 
     @Override
@@ -44,6 +152,53 @@ public class MainActivity extends Activity {
         return true;
     }
     
+//    public void clickIt(){
+//    	Log.i("TEST", "ClickIt");
+//    	
+//    	switch (webview.getId()) {
+//    	case R.id.btn1:
+//    		myToast("Button 1");
+//    		break;
+//    	case R.id.btn2:
+//    		
+//    		break;
+//    	case R.id.btn3:
+//    		
+//    		break;
+//    	case R.id.btn4:
+//    		
+//    		break;
+//    	case R.id.btn5:
+//    		
+//    		break;
+//    	case R.id.btn6:
+//    		
+//    		break;
+//    	case R.id.btn7:
+//    		
+//    		break;
+//    	case R.id.btn8:
+//    		
+//    		break;
+//    	case R.id.btn9:
+//    		
+//    		break;
+//    	case R.id.btn0:
+//    		
+//    		break;
+//    	default:
+//    		break;
+//    	}
+//    	
+//    }// END setinfo
+    
+    public void myToast(String text){  // Toast Template
+		CharSequence textIN = text;
+		int duration = Toast.LENGTH_SHORT;
+		Toast toast = Toast.makeText(MainActivity.this, textIN, duration);
+		toast.setGravity(Gravity.BOTTOM, 0, 0);
+		toast.show();
+	};// end myToast
 
     
 
@@ -111,44 +266,6 @@ public class MainActivity extends Activity {
 			
 		}
 		
-//		public void setInfo(){
-//			
-//			switch (webview.getId()) {
-//			case R.id.btn1:
-//				
-//				break;
-//			case R.id.btn2:
-//							
-//				break;
-//			case R.id.btn3:
-//				
-//				break;
-//			case R.id.btn4:
-//				
-//				break;
-//			case R.id.btn5:
-//				
-//				break;
-//			case R.id.btn6:
-//				
-//				break;
-//			case R.id.btn7:
-//				
-//				break;
-//			case R.id.btn8:
-//				
-//				break;
-//			case R.id.btn9:
-//				
-//				break;
-//			case R.id.btn0:
-//				
-//				break;
-//			default:
-//				break;
-//			}
-//			
-//		}// END setinfo
 	
 	}  //  END Class
 }  // END MAINACTIVITY
